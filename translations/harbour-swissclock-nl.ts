7ae4870e746190057a37415caf0130d985fa7329
d9h20f <>
2018-01-30 14:39:52 +0100

    Dutch translation

---
+++ translations/harbour-swissclock-nl.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name></name>
+    <message id="swissclock-title-swiss_railroad">
+        <source>Swiss railroad</source>
+        <extracomment>Label for Swiss raiload clock style</extracomment>
+        <translation>Zwitserse spoorwegen</translation>
+    </message>
+    <message id="swissclock-title-helsinki_metro">
+        <source>Helsinki metro</source>
+        <extracomment>Label for Helsinki metro clock style</extracomment>
+        <translation>Metro in Helsinki</translation>
+    </message>
+    <message id="swissclock-title-deutsche_bahn">
+        <source>Deutsche Bahn</source>
+        <extracomment>Label for Deutsche Bahn clock style</extracomment>
+        <translation>Deutsche Bahn</translation>
+    </message>
+    <message id="swissclock-settings-page_header">
+        <source>Swiss Clock</source>
+        <extracomment>Settings page header</extracomment>
+        <translation>Zwitserse klok</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate">
+        <source>Dial plate</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Wijzerplaat</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-description">
+        <source>You can also switch between light and dark backgrounds by double-tapping the dial plate</source>
+        <extracomment>Combo box description</extracomment>
+        <translation>Je kan ook wisselen tussen lichte en donkere achtergronden door te dubbeltikken op de wijzerplaat</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-value-light">
+        <source>Light</source>
+        <extracomment>Combo box value for the normal (light) dial plate</extracomment>
+        <translation>Licht</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-value-dark">
+        <source>Dark</source>
+        <extracomment>Combo box value for inverted (dark) dial plate</extracomment>
+        <translation>Donker</translation>
+    </message>
+    <message id="swissclock-settings-orientation_label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Oriëntatie</translation>
+    </message>
+    <message id="swissclock-settings-orientation-primary">
+        <source>Primary</source>
+        <extracomment>Combo box value for primary orientation</extracomment>
+        <translation>Primair</translation>
+    </message>
+    <message id="swissclock-settings-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Portret</translation>
+    </message>
+    <message id="swissclock-settings-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Landschap</translation>
+    </message>
+    <message id="swissclock-settings-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dynamisch</translation>
+    </message>
+    <message id="swissclock-settings-orientation-allow_inverted">
+        <source>Allow inverted orientation</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Omgekeerde oriëntatie toestaan</translation>
+    </message>
+    <message id="swissclock-settings-orientation-allow_inverted-description">
+        <source>If enabled, allows both primary and inverted landscape or portrait oprientations</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Indien ingeschakeld worden zowel primaire als omgekeerde landschaps- of portretoriëntaties toegestaan</translation>
+    </message>
+</context>
+</TS>
