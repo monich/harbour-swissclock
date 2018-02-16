ceb7d3b4a475ef4574427b595b564c5b2a2b95ce
1Zgp <36508793+1Zgp@users.noreply.github.com>
2018-02-16 17:08:01 +0200

    Create harbour-swissclock-hu.ts
    Hungarian translation file for Swiss Clock. Please add it to your app. Thanks.

---
+++ translations/harbour-swissclock-hu.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="swissclock-title-swiss_railroad">
+        <source>Swiss railroad</source>
+        <extracomment>Label for Swiss raiload clock style</extracomment>
+        <translation>Svájci vasút</translation>
+    </message>
+    <message id="swissclock-title-helsinki_metro">
+        <source>Helsinki metro</source>
+        <extracomment>Label for Helsinki metro clock style</extracomment>
+        <translation>Helsinki-i metró</translation>
+    </message>
+    <message id="swissclock-title-deutsche_bahn">
+        <source>Deutsche Bahn</source>
+        <extracomment>Label for Deutsche Bahn clock style</extracomment>
+        <translation>Német vasút</translation>
+    </message>
+    <message id="swissclock-settings-page_header">
+        <source>Swiss Clock</source>
+        <extracomment>Settings page header</extracomment>
+        <translation>Svájci óra</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate">
+        <source>Dial plate</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Számlap</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-description">
+        <source>You can also switch between light and dark backgrounds by double-tapping the dial plate</source>
+        <extracomment>Combo box description</extracomment>
+        <translation>A világos és sötét hátterek közt válthatsz a számlap dupla érintésével is.</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-value-light">
+        <source>Light</source>
+        <extracomment>Combo box value for the normal (light) dial plate</extracomment>
+        <translation>Világos</translation>
+    </message>
+    <message id="swissclock-settings-dial_plate-value-dark">
+        <source>Dark</source>
+        <extracomment>Combo box value for inverted (dark) dial plate</extracomment>
+        <translation>Sötét</translation>
+    </message>
+    <message id="swissclock-settings-orientation_label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Tájolás</translation>
+    </message>
+    <message id="swissclock-settings-orientation-primary">
+        <source>Primary</source>
+        <extracomment>Combo box value for primary orientation</extracomment>
+        <translation>Elsődleges</translation>
+    </message>
+    <message id="swissclock-settings-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Álló</translation>
+    </message>
+    <message id="swissclock-settings-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Fekvő</translation>
+    </message>
+    <message id="swissclock-settings-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dinamikus</translation>
+    </message>
+    <message id="swissclock-settings-orientation-allow_inverted">
+        <source>Allow inverted orientation</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Fordított tájolás engedélyezése</translation>
+    </message>
+    <message id="swissclock-settings-orientation-allow_inverted-description">
+        <source>If enabled, allows both primary and inverted landscape or portrait oprientations</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Ha engedélyezve van, mind az elsődleges, mind a fordított fekvő vagy álló tájolást lehetővé teszi.</translation>
+    </message>
+</context>
+</TS>
+
+    © 2018 GitHub, Inc.
